74a063956ca4491becbbce6fb3731c9a2b64a535
Germán <german.palencia@adentia.es>
2023-08-28 13:27:37 +0200

    Task - Search by country and search by region

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
@@ -14,8 +14,28 @@ export class CountriesService {
 
   searchCapital(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${term}`;
-    return this.http.get<Country[]>(url);
+
+    return this.http.get<Country[]>(url)
+    //Capturar errores a través del pipe
+    .pipe(
+      catchError(() => of([]))
+    );
+  }
+
+  searchCoutry(term: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/name/${term}`
+    return this.http.get<Country[]>(url)
+    .pipe(
+      catchError(() => of([]))
+    );
+  }
+
+  searchRegion(region: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/region/${region}`
+    return this.http.get<Country[]>(url)
+    .pipe(
+      catchError(() => of([]))
+    );
   }
 
 }
-
